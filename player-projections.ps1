88db0761ef21f91e9f56e9584308028900905f16
Brandon Quinn <github@thequinn.org>
2024-12-30 21:26:26 -0600

    Added function

--- player-projections.ps1
+++ player-projections.ps1
@@ -1,12 +1,31 @@
+function Get-PlayerInfo {
+    param (
+        [array]$players
+    )
+    foreach ($player in $players) {
+        $batters | Where-Object {$_.PlayerName -eq $player.Player}
+    }
+}
+
+$availableBattersPlusMe = Import-Csv -Path 'C:\users\shatt\Downloads\fantrax\Batters-Available+Me.csv'
+$availableStartersPlusMe = Import-Csv -Path 'C:\users\shatt\Downloads\fantrax\Starters-Available+Me.csv'
+$availableRelieversPlusMe = Import-Csv -Path 'C:\users\shatt\Downloads\fantrax\Relievers-Available+Me.csv'
+
+#all players
+$allPlayers
+
 #batters
-$battersUrl = 'https://www.fangraphs.com/api/projections?type=atc&stats=bat&pos=all'
+$battersUrl = 'https://www.fangraphs.com/api/projections?type=steamer&stats=bat&pos=all'
 $batters = Invoke-RestMethod -Uri $battersUrl -Method Get
-$batters | Select-Object PlayerName, minpos, WAR, sb, RBI, R, OBP | Select-Object -First 3
 
 #starters
 $startersUrl = 'https://www.fangraphs.com/api/projections?type=steamer&stats=sta&pos=all'
 $starters = Invoke-RestMethod -Uri $startersUrl -Method Get
 
 #relievers
-$relieversUrl = 'https://www.fangraphs.com/api/projections?type=steamer&stats=rel&pos=all'
+$relieversUrl = 'https://www.fangraphs.com/api/projections?type=steamer&stats=pit&pos=all'
 $relievers = Invoke-RestMethod -Uri $relieversUrl -Method Get
+
+foreach ($player in $players) {
+    $batters | Where-Object {$_.PlayerName -eq $player} | Select-Object PlayerName
+}
